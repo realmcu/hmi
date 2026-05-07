b9fbeaa9e15a5079fecbe54e09d6bfaa056326bd
howie_wang <howie_wang@realsil.com.cn>
2026-05-07 15:38:42 +0800

    Add simple project
    Change-Id: If21e0ae916c27800705bfa892eec227854d8f131

---
+++ deskmate/main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include "trace.h"
+#include "rtl876x_pinmux.h"
+
+int main(void)
+{
+    DBG_DIRECT("!!!!! remap log pin!!!");
+    Pinmux_Config(P2_0, IDLE_MODE);
+    Pinmux_Config(P3_1, UART1_TX);
+    Pad_Config(P3_1, PAD_PINMUX_MODE, PAD_IS_PWRON, PAD_PULL_NONE, PAD_OUT_DISABLE, PAD_OUT_LOW);
+    DBG_DIRECT("!!!!! remap log pin!!!");
+    printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+    return 0;
+}
