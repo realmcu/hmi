1f130a37e3c6a6d5cbdbb019641b8db52384ee01
luke_sun <luke_sun@realsil.com.cn>
2026-05-22 10:04:55 +0800

    Dashboard: remove menifest chargecase west
    Change-Id: I6971a969fc6cb37dd19dba3095daa1f7b42ee582

--- chargecase/mdk/rtl87x3ep_before_build_script.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set flash_bank_path=%1
-set ic_type=%2
-set target=%3
-
-
-copy ..\..\..\..\..\bin\%ic_type%\flash_map_config\%flash_bank_path%\flash_%flash_bank_path%\flash_map.h ..\..\..\..\..\bin\%ic_type%\flash_map_config\flash_map.h
-copy ..\..\..\..\..\bin\%ic_type%\upperstack_stamp\%flash_bank_path%\upperstack_compile_stamp.h ..\..\..\..\..\bin\%ic_type%\upperstack_stamp\upperstack_compile_stamp.h
