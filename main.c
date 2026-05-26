06be78907ccb13da4423f6514fdfbd30191a130a
howie_wang <howie_wang@realsil.com.cn>
2026-05-26 11:53:33 +0800

    add .vscode project
    Change-Id: I6f263b22c96ff3923adde249c2b95a4a2e081bf0

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int main(void)
     Pad_Config(P3_1, PAD_PINMUX_MODE, PAD_IS_PWRON, PAD_PULL_NONE, PAD_OUT_DISABLE, PAD_OUT_LOW);
     DBG_DIRECT("!!!!! remap log pin!!!");
 #endif
-    app_system_lower_init();
+    // app_system_lower_init();
     printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
     printf("[main] thread id: %p, name: %s, priority: %d\n",
            k_current_get(),
@@ -32,7 +32,7 @@ int main(void)
     hmi_bt_task_init();
     hmi_proto_task_init();
 
-    gui_server_init();
+    // gui_server_init();
 
     return 0;
 }
