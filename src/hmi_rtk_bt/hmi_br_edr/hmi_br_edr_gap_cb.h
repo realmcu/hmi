ec93957f1116a3c568caa17ff0be280892fdcb80
luke_sun <luke_sun@realsil.com.cn>
2026-05-27 18:07:26 +0800

    Dashboard: add hmi_rtk_bt
    Change-Id: I3df60c9fc4c639796747a1d3b9e893af56a1f6b7

---
+++ src/hmi_rtk_bt/hmi_br_edr/hmi_br_edr_gap_cb.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018, Realsil Semiconductor Corporation. All rights reserved.
+ */
+
+#ifndef _HMI_BR_EDR_GAP_CB_H_
+#define _HMI_BR_EDR_GAP_CB_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+#include "stdbool.h"
+#include "btm.h"
+
+
+void hmi_br_edr_gap_bt_cback(T_BT_EVENT event_type, void *event_buf, uint16_t buf_len);
+void hmi_br_edr_gap_common_cb(uint8_t cb_type, void *p_cb_data);
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _HMI_BR_EDR_GAP_CB_H_ */
